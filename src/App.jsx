108309f68b5ddc2908dda35ac423272f56ffed98
Adrisa <adrisansyarif@gmail.com>
2024-07-09 18:37:43 +0700

    feat: Category UI in category page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { GenderPage } from "./pages/categoryGender/gender";
 import { HomePages } from "./pages/Home";
+import { Categories } from "./pages";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePages />} />
-                {/* <Route path="/gender" element={<GenderPage />} /> */}
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/category" element={<Categories />} />
+        {/* <Route path="/gender" element={<GenderPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      categoryYellow: "#F9E29A",
+      black: "#000",
+      white: "#ffffff",
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-gradient": {
+          "background-image": "linear-gradient(to right, #F3C11A, #FFFFFF)",
+          "-webkit-background-clip": "text",
+          "-webkit-text-fill-color": "transparent",
+        },
+      };
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    }),
+  ],
 };
