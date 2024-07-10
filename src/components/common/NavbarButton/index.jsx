531f35ff2d2e5c041fee9cf8ec30b285d28b55f8
Abk1708 <bgus1708@gmail.com>
2024-07-10 12:03:27 +0700

    feat: add homepage

---
+++ src/components/common/NavbarButton/index.jsx
@@ -0,0 +1,22 @@
+import { Button } from "../Button";
+import { Card } from "../Card";
+import homeImg from "../../../assets/image//Home.png";
+import listImg from "../../../assets/image//List.png";
+
+export function NavButton() {
+    return (
+        <div className="flex justify-center sticky bottom-10 font-medium text-lg text-stone-900">
+            <Card className="border-slate-100 shadow-md rounded-full flex w-fit justify-around text-center items-center bg-white p-5">
+                <Button className="bg-transparent border-none flex items-center gap-2">
+                    <img src={homeImg} alt="" className="size-4" />
+                    <p>Home</p>
+                </Button>
+                <span></span>
+                <Button className="bg-transparent border-none flex items-center gap-2 ">
+                    <img src={listImg} alt="" className="size-4" />
+                    <p>Category</p>
+                </Button>
+            </Card>
+        </div>
+    );
+}
