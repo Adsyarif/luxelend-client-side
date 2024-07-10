7522ff968b5240b20e0224e294e7a5fe9fef745d
smmuja <smmuja27@gmail.com>
2024-07-10 23:26:03 +0700

    fix: import path

--- src/components/Home/BrandLineup/index.jsx
+++ src/components/Home/BrandLineup/index.jsx
@@ -5,35 +5,27 @@ import tedBaker from "../../../assets/image/ted-baker.webp";
 import reiss from "../../../assets/image/reiss.webp";
 
 export function BrandLineup() {
-    return (
-        <Card className="pb-4">
-            <p className="py-2 mx-4 font-bold">Brand</p>
-            <div className="flex flex-row mx-8 gap-6 text-xs font-semibold text-center mx-3">
-                <div>
-                    <img
-                        className="h-12 ml-2 my-1"
-                        src={kateSpade}
-                        alt="Kate Spade"
-                    />
-                    <p>Kate Spade</p>
-                </div>
-                <div>
-                    <img
-                        className="pl-2 h-14"
-                        src={michaelKors}
-                        alt="Michael Kors"
-                    />
-                    <p>Michael Kors</p>
-                </div>
-                <div>
-                    <img className="pt-4 pb-3" src={tedBaker} alt="Ted Baker" />
-                    <p>Ted Baker</p>
-                </div>
-                <div>
-                    <img className="pt-6 pb-5" src={reiss} alt="Reiss" />
-                    <p>Reiss</p>
-                </div>
-            </div>
-        </Card>
-    );
+  return (
+    <Card className="pb-4">
+      <p className="py-2 mx-4 font-bold">Brand</p>
+      <div className="flex flex-row mx-8 gap-6 text-xs font-semibold text-center">
+        <div>
+          <img className="h-12 ml-2 my-1" src={kateSpade} alt="Kate Spade" />
+          <p>Kate Spade</p>
+        </div>
+        <div>
+          <img className="pl-2 h-14" src={michaelKors} alt="Michael Kors" />
+          <p>Michael Kors</p>
+        </div>
+        <div>
+          <img className="pt-4 pb-3" src={tedBaker} alt="Ted Baker" />
+          <p>Ted Baker</p>
+        </div>
+        <div>
+          <img className="pt-6 pb-5" src={reiss} alt="Reiss" />
+          <p>Reiss</p>
+        </div>
+      </div>
+    </Card>
+  );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,29 @@
-import { Header } from "../../components/common";
-import { Card } from "../../components/common/Card";
-import { WelcomeBanner } from "../../components/Home/WelcomeBanner";
-import { Button } from "../../components/common/Button";
-import { HomeBanner } from "../../components/Home/HomeBanner";
-import { BrandLineup } from "../../components/Home/BrandLineup";
-import { FeaturesCard } from "../../components/Home/FeaturesCard";
-import { NavButton } from "../../components/common/NavbarButton";
+import { Header } from "@/components";
+import { Card } from "@/components/common";
+import { WelcomeBanner } from "@/components/Home";
+import { Button } from "@/components";
+import { HomeBanner } from "@/components/Home";
+import { BrandLineup } from "@/components/Home";
+import { FeaturesCard } from "@/components/Home";
+import { NavButton } from "@/components";
 
 export function HomePages() {
-    return (
-        <Card>
-            <Header />
-            <WelcomeBanner />
-            <div className=" flex flex-row justify-center align-middle font-semibold gap-10 my-4">
-                <Button className="bg-rose-50 w-28">
-                    <p>Women</p>
-                </Button>
-                <Button className="bg-rose-50 w-28">
-                    <p>Men</p>
-                </Button>
-            </div>
-            <HomeBanner />
-            <BrandLineup />
-            <FeaturesCard />
-            <NavButton />
-        </Card>
-    );
+  return (
+    <Card>
+      <Header />
+      <WelcomeBanner />
+      <div className=" flex flex-row justify-center align-middle font-semibold gap-10 my-4">
+        <Button className="bg-rose-50 w-28">
+          <p>Women</p>
+        </Button>
+        <Button className="bg-rose-50 w-28">
+          <p>Men</p>
+        </Button>
+      </div>
+      <HomeBanner />
+      <BrandLineup />
+      <FeaturesCard />
+      <NavButton />
+    </Card>
+  );
 }
