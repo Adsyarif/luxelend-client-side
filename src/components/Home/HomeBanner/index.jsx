12db547f1259d14a76fa419452eb1d88170fae67
Abk1708 <bgus1708@gmail.com>
2024-07-10 12:03:27 +0700

    feat: add homepage

---
+++ src/components/Home/HomeBanner/index.jsx
@@ -0,0 +1,7 @@
+import { Card } from "../../common/Card";
+
+export function HomeBanner() {
+    return (
+        <Card className="m-0 border-none  flex  h-52 flex-col  bg-center bg-no-repeat bg-cover bg-[url('src/assets/image/HomeBanner.webp')]" />
+    );
+}
