11b919c108c4b5d66b4e56d6635f3f9d83901d67
Abk1708 <bgus1708@gmail.com>
2024-07-10 21:41:30 +0700

    refactor: remove duplicate assets

--- src/pages/Categories/index.jsx
+++ src/pages/Categories/index.jsx
@@ -1,27 +1,27 @@
-import { CategoryCard } from "../../components/categories";
+import { CategoryCard } from "../../components/Categories";
 import { Header, BackButton } from "../../components/common";
-import { useState } from "react";
+// import { useState } from "react";
 import data from "../../data/categoryData.json";
 
 const Categories = () => {
-  // console.log(data.gender.men);
-  const [Categories, setCategories] = useState({
-    women: [],
-    men: [],
-  });
-  const fetchCategory = async () => {
-    const response = fetch("../../data/categoryData.json");
-    const data = response.json();
-  };
+    // console.log(data.gender.men);
+    // const [Categories, setCategories] = useState({
+    //   women: [],
+    //   men: [],
+    // });
+    // const fetchCategory = async () => {
+    //   const response = fetch("../../data/categoryData.json");
+    //   const data = response.json();
+    // };
 
-  return (
-    <div className="max-w-screen-sm mx-auto md:max-w-2xl">
-      <Header />
-      <BackButton isText={true} />
-      <CategoryCard items={data.category.women} gender={"Women"} />
-      <CategoryCard items={data.category.men} gender={"Men"} />
-    </div>
-  );
+    return (
+        <div className="max-w-screen-sm mx-auto md:max-w-2xl">
+            <Header />
+            <BackButton isText={true} />
+            <CategoryCard items={data.category.women} gender={"Women"} />
+            <CategoryCard items={data.category.men} gender={"Men"} />
+        </div>
+    );
 };
 
 export default Categories;
