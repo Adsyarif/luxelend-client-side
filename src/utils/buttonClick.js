5a03450692f3102ede47f6c0f27efae082bfa057
Adrisa <adrisansyarif@gmail.com>
2024-07-10 16:55:10 +0700

    fix: banner reusable component category and product

--- src/utils/buttonClick.js
+++ src/utils/buttonClick.js
@@ -10,4 +10,20 @@ export const selectGender = (gender) => {
   }));
 };
 
-export const navigateClick = () => {};
+export const upperCaseFirstCharacter = (text) => {
+  try {
+    if (typeof text !== "string") {
+      throw new TypeError("Input must be a string");
+    }
+    if (!text) return text;
+
+    const firstChar = text[0].toUpperCase();
+    const restOfString = text.slice(1);
+    const newUpperCaseText = firstChar + restOfString;
+
+    return newUpperCaseText;
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+    return null; // or you can choose to return a default value
+  }
+};
