5ff8b1838550e7e886519d4a9af5700aaffd5114
Abk1708 <bgus1708@gmail.com>
2024-07-10 19:11:08 +0700

    style: adjust welcome banner font size

--- src/components/Home/WelcomeBanner/index.jsx
+++ src/components/Home/WelcomeBanner/index.jsx
@@ -3,7 +3,7 @@ import LuxelendLogo from "../../../assets/image/luxelend-logo.webp";
 
 export function WelcomeBanner() {
     return (
-        <Card className="m-0 border-none flex flex-row text-center gap-16 justify-center items-center h-16 text-white bg-gradient-to-r from-gray-900 from-40% to-blue-900">
+        <Card className="m-0 border-none flex flex-row text-center text-2xl gap-4 justify-center items-center h-16 text-white bg-gradient-to-r from-gray-900 from-40% to-blue-900">
             <img className="h-14 w-14" src={LuxelendLogo} alt="LuxlendLogo" />
             <i>Welcome to LuxeLend!</i>
         </Card>
