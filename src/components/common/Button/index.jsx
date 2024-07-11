51984360026434301cae38d96a9429f67e06f730
Abk1708 <bgus1708@gmail.com>
2024-07-11 17:10:10 +0700

    style: change Card as wrapper to div merge main to layout/homePage

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -1,8 +1,9 @@
 import { twMerge } from "tailwind-merge";
 
-export function Button({ children, className }) {
+export function Button({ children, className, handleClick }) {
     return (
         <button
+            onClick={handleClick}
             className={twMerge(
                 "rounded-md px-3 py-1 border border-slate-100 bg-yellow-600",
                 className
